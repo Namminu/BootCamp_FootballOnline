77eb531a3dc9319850d095509586d2973f214e05
dwook <dwook1637@gmail.com>
2024-12-03 16:19:26 +0900

    app.js listen() 중복 제거

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 // import PlayerRouter from "./routes/player.router.js";
 import PlayerDrawRouter from "./routes/player.draw.router.js";
 // import PlayerEnhanceRouter from "./routes/player.enhance.router.js";
-// import RankingRouter from "./routes/ranking.router.js";
+import RankingRouter from "./routes/ranking.router.js";
 //import dotenv from "dotenv";
 //import errorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
 
@@ -29,16 +29,6 @@ app.use("/api", [
 ]);
 
 //dotenv.config();
-
-app.get("/", (req, res) => {
-  //const { authorization: token } = req.headers;
-
-  res.send("풋살 온라인 게임 서버 서비스입니다.");
-});
-
-app.listen(PORT, () => {
-  console.log(PORT, "포트로 서버가 열렸어요!");
-
 
 // 서버 시작
 app.listen(PORT, () => {
