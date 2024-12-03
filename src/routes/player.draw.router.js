ecb46e44a4cce6e0816cf01804a628c207e9586c
dwook <dwook1637@gmail.com>
2024-12-03 16:31:34 +0900

    player.draw. 두번째 커밋
    주석 및 불필요 코드 정리

--- src/routes/player.draw.router.js
+++ src/routes/player.draw.router.js
@@ -1,7 +1,6 @@
 import express from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import { prisma } from "../utils/prisma/index.js";
-import { Prisma } from "@prisma/client";
 
 const router = express.Router();
 
@@ -9,42 +8,25 @@ const router = express.Router();
 router.post("/players/draw", authMiddleware, async (req, res, next) => {
   try {
     const account = req.account;
-    const drawPrice = 1000;
+    const cost = 1000;
 
     //캐시 보유 1000원 이상인지 검사
-    if (account.cash < drawPrice) {
+    if (account.cash < cost) {
       return res.status(400).json({ Message: "보유 캐쉬가 부족합니다." });
     }
 
-    const randomtierList = [tier0, tier1, tier2, tier3, tier4, tier5];
-
-    //랜덤 등급
-    const randomNumRarity = Math.random();
-
-    let rarity = 0;
-    for (const element in randomtierList) {
-      let currentrandomNum = 0;
-      for (let i = 0; i <= element; i++) {
-        currentrandomNum += randomtierList[i];
-      }
-      if (randomNumRarity < currentrandomNum) {
-        rarity = +element;
-        break;
-      }
-    }
-
+    // 선수 뽑기 로직
     do {
-      //선수 테이블에서 랜덤 선수 가져오기
+      // 선수 테이블에서 랜덤 선수 가져오기
       const players = await prisma.players.findMany({});
-      const randomNum = Math.floor(Math.random() * players.length); //랜덤으로 뽑을 리스트 idex 뽑기
-      const randomPlayer = players[randomNum]; //당첨 선수 정보
-
-      //
+      const randomNum = Math.floor(Math.random() * players.length);
+      const randomPlayer = players[randomNum];
+      
       if (!randomPlayer) {
-        return res.status(400).json({ Message: "존재하지 않는 선수입니다." });
+        return res.status(404).json({ Message: "존재하지 않는 선수입니다." });
       }
 
-      //캐릭터 보유 선수중 뽑힌 캐릭터가 있는지
+      // 캐릭터 보유 선수중 뽑힌 캐릭터가 있는지
       let existingPlayer = await prisma.myPlayers.findFirst({
         where: {
           player_id: randomPlayer.player_id,
@@ -53,13 +35,14 @@ router.post("/players/draw", authMiddleware, async (req, res, next) => {
       // 중복이면 50% 확률로 다시 뽑게 하기
     } while (existingPlayer && Math.random() * 2 < 1);
 
+    // 캐쉬 차감 및 보유 선수 업데이트 (트랜잭션)
     const newPlayer = await prisma.$transaction(
       async (tx) => {
         //캐쉬 차감
         await tx.accounts.update({
           where: { account_id: account.account_id },
           data: {
-            money: account.money - drawPrice,
+            money: account.money - cost,
           },
         });
 
@@ -75,7 +58,7 @@ router.post("/players/draw", authMiddleware, async (req, res, next) => {
         return newPlayer;
       },
       {
-        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted, // 격리 레벨
+        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
       }
     );
 
