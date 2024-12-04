4232cea01e5b8364a6830ef519556f2c97f0967b
황종하 <whdgk3615@naver.com>
2024-12-04 14:01:43 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-// import AccountsRouter from "./routes/accounts.router.js";
+import AccountsRouter from "./routes/accounts.router.js";
 // import AccountPlayerRouter from "./routes/account.player.router.js";
 // import AccountSquadRouter from "./routes/account.squad.router.js";
 // import GameRouter from "./routes/game.router.js";
@@ -20,12 +20,11 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-//app.use(errorHandlingMiddleware);
 app.use("/api", [
-//   AccountsRouter,
+   AccountsRouter,
 //   AccountPlayerRouter,
 //   AccountSquadRouter,
-//   GameRouter,
+  GameRouter,
 //   PlayerRouter,
    PlayerDrawRouter,
    PlayerEnhanceRouter,
