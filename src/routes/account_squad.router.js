f45db4471a087e8ffd335075025deb2a1cd98d94
Namminu <mvpalsdn771@gmail.com>
2024-12-04 19:44:38 +0900

    api 수정 및 로직 설명 주석 추가
    이후 작업에서 작성한 주석을 보면서 코드 구성할 예정

--- src/routes/account_squad.router.js
+++ src/routes/account_squad.router.js
@@ -81,9 +81,25 @@ router.get('/squad/:targetId', authMiddleware, async (req, res, next) => {
 });
 
 // 스쿼드 추가 API
-router.post('/squad/:playerId', middleware, async (req, res, next) => {
+router.post('/squad/:playerId/setup', authMiddleware, async (req, res, next) => {
     try {
+        if (!req.account) return res.status(401).json({ message: "로그인이 필요합니다." });
+        const accountId = +req.account.accountId;
+        // accountId 기반으로 MyPlayers 테이블에서 보유 선수 > 0 인지 탐색
+        // 보유 선수 >= 3 이면 return 예외 처리
 
+        // 보유한 선수가 없을 경우 return 예외 처리
+
+        // 있을 경우 params 로 전달한 스쿼드에 추가하려는 선수 할당
+        const playerId = +req.params.playerId;
+        // 선수 id를 다시 MyPlayers 테이블에서 탐색, 보유한 선수인지 조회
+        //if(!playerId) return res.status(404).json({message : "현재 해당 선수를 보유하고 있지 않습니다."});
+
+        // 있을 경우 Squad 테이블로 전달 : player1, 2, 3 순으로 순차 전달해야됨
+
+        // 이후 종료
+        const message = `${playerId.player_name} 선수를 스쿼드에 등록했습니다`;
+        return res.status(200).json(message);
     } catch (err) {
         console.log(err);
         return res.status(500).json({
@@ -94,7 +110,7 @@ router.post('/squad/:playerId', middleware, async (req, res, next) => {
 });
 
 // 스쿼드 삭제 API
-router.delete('/squad/:playerId', middleware, async (req, res, next) => {
+router.delete('/squad/:playerId/setdown', authMiddleware, async (req, res, next) => {
     try {
 
     } catch (err) {
